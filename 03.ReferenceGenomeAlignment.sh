1b63c385afd6b7a7bb1c865afe1b2db87ce67f0e
Hartecky <hofman.bartlomiej@gmail.com>
2022-05-04 09:27:06 +0200

    prepared indexing and alignment with BWA MEM algorithm

--- 03.ReferenceGenomeAlignment.sh
+++ 03.ReferenceGenomeAlignment.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 
 # ----------------------------------------
+# PROJECT NAMES
+
+PROJECTS=(
+	"SRR064547"
+	"SRR064546"
+	"SRR064545"
+	)
+
+# ----------------------------------------
 # NAMES
+
+FORWARD="_1.fastq.gz"
+REVERSE="_2.fastq.gz"
 
 REFERENCEGENOME="Saccharomyces.reference.genome.fa.gz"
 
 bwa index "$REFERENCEGENOME"
+
+for SAMPLE in "${PROJECTS[@]}"
+do
+
+	bwa mem \
+	-t 4 \
+	 "$REFERENCEGENOME" \
+	 "../qc_data/${SAMPLE}$FORWARD" \
+	 "../qc_data/${SAMPLE}$REVERSE" | \
+	 samtools view -@ 4 \
+	 -Sb > "../alignment/${SAMPLE}.bam"
+
+done
